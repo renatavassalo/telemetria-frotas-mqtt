70492b506d69fc36da4591fc713f3eed5da9a1f4
renatavassalo <121727905+renatavassalo@users.noreply.github.com>
2026-07-02 20:16:33 -0300

    feat: implementa envio de telemetria em formato JSON

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -1,15 +1,33 @@
 import random
 import time
+import json
+from datetime import datetime
+
 import paho.mqtt.client as mqtt
+
 from config.mqtt_config import *
 
 client = mqtt.Client(client_id=CLIENT_ID_PUBLISHER)
 client.connect(BROKER, PORT)
 
-print("🚚 Caminhão Cegonha na estrada... Enviando telemetria.")
+print("🚚 Caminhão na estrada... Enviando telemetria.")
 
 while True:
-    velocidade = random.randint(60, 110)
-    client.publish(TOPIC, velocidade)
-    print(f"Velocidade enviada: {velocidade} km/h")
+    payload = {
+        "veiculo": "TRUCK-001",
+        "placa": "ABC-1234",
+        "velocidade": random.randint(60, 110),
+        "latitude": -29.765432,
+        "longitude": -51.153214,
+        "timestamp": datetime.now().isoformat()
+    }
+
+    client.publish(
+        TOPIC,
+        json.dumps(payload)
+    )
+
+    print("📤 Telemetria enviada:")
+    print(json.dumps(payload, indent=4, ensure_ascii=False))
+
     time.sleep(3)
--- subscriber/subscriber.py
+++ subscriber/subscriber.py
@@ -1,9 +1,26 @@
+import json
 import paho.mqtt.client as mqtt
+
 from config.mqtt_config import *
 
 def on_message(client, userdata, msg):
-    velocidade = int(msg.payload.decode())
-    print(f"📡 Telemetria recebida | Velocidade: {velocidade} km/h")
+    data = json.loads(msg.payload.decode())
+
+    veiculo = data["veiculo"]
+    placa = data["placa"]
+    velocidade = data["velocidade"]
+    latitude = data["latitude"]
+    longitude = data["longitude"]
+    timestamp = data["timestamp"]
+
+    print("=" * 50)
+    print("🚚 NOVA TELEMETRIA RECEBIDA")
+    print(f"Veículo.....: {veiculo}")
+    print(f"Placa.......: {placa}")
+    print(f"Velocidade..: {velocidade} km/h")
+    print(f"Latitude....: {latitude}")
+    print(f"Longitude...: {longitude}")
+    print(f"Horário.....: {timestamp}")
 
 client = mqtt.Client(client_id=CLIENT_ID_SUBSCRIBER)
 
