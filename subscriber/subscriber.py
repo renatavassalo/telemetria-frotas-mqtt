86a347a866a07694cd19f1e9ac6ec97cf2b009c2
renatavassalo <121727905+renatavassalo@users.noreply.github.com>
2026-07-11 20:14:11 -0300

    feat: adiciona alerta de excesso de velocidade e QoS 1

--- subscriber/subscriber.py
+++ subscriber/subscriber.py
@@ -3,6 +3,9 @@ import paho.mqtt.client as mqtt
 
 from config.mqtt_config import *
 
+LIMITE_VELOCIDADE = 90
+
+
 def on_message(client, userdata, msg):
     data = json.loads(msg.payload.decode())
 
@@ -22,13 +25,22 @@ def on_message(client, userdata, msg):
     print(f"Longitude...: {longitude}")
     print(f"Horário.....: {timestamp}")
 
+    if velocidade > LIMITE_VELOCIDADE:
+        print("\n🚨 ALERTA DE EXCESSO DE VELOCIDADE!")
+        print(f"Veículo: {veiculo}")
+        print(f"Velocidade atual: {velocidade} km/h")
+        print(f"Limite permitido: {LIMITE_VELOCIDADE} km/h")
+    else:
+        print("\n✅ Velocidade dentro do limite.")
+
+
 client = mqtt.Client(client_id=CLIENT_ID_SUBSCRIBER)
 
 client.on_message = on_message
 
 client.connect(BROKER, PORT)
 
-client.subscribe(TOPIC)
+client.subscribe(TOPIC, qos=1)
 
 print("🖥️ Central de Monitoramento Ativa. Aguardando dados da frota...")
 
