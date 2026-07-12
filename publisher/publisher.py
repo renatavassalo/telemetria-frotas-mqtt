71c233272fde07d7d59a21ffffcb67fd041376b2
renatavassalo <121727905+renatavassalo@users.noreply.github.com>
2026-07-11 21:52:19 -0300

    feat: adiciona simulacao de multiplos veiculos na frota

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -7,15 +7,33 @@ import paho.mqtt.client as mqtt
 
 from config.mqtt_config import *
 
+VEICULOS = [
+    {
+        "veiculo": "TRUCK-001",
+        "placa": "ABC-1234"
+    },
+    {
+        "veiculo": "TRUCK-002",
+        "placa": "XYZ-5678"
+    },
+    {
+        "veiculo": "TRUCK-003",
+        "placa": "DEF-9012"
+    }
+]
+
 client = mqtt.Client(client_id=CLIENT_ID_PUBLISHER)
 client.connect(BROKER, PORT)
 
-print("🚚 Caminhão na estrada... Enviando telemetria.")
+print("🚚 Frota na estrada... Enviando telemetria.")
 
 while True:
+
+    veiculo_atual = random.choice(VEICULOS)
+
     payload = {
-        "veiculo": "TRUCK-001",
-        "placa": "ABC-1234",
+        "veiculo": veiculo_atual["veiculo"],
+        "placa": veiculo_atual["placa"],
         "velocidade": random.randint(60, 110),
         "latitude": -29.765432,
         "longitude": -51.153214,
@@ -27,8 +45,8 @@ while True:
     client.publish(
         topic_veiculo,
         json.dumps(payload),
-         qos=1
-)
+        qos=1
+    )
 
     print("📤 Telemetria enviada:")
     print(json.dumps(payload, indent=4, ensure_ascii=False))
